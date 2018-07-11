3b5122535566e292ea5448f9688cfb44e1117988
Sakshi <sakshisrivastava413@gmail.com>
2018-07-12 02:10:17 +0530

    adding task

--- main.js
+++ main.js
@@ -8,3 +8,35 @@ inputVal.addEventListener("keyup", function (event) {
     document.getElementById("adding").click();
   }
 });
+
+var todos = [];
+
+
+function addTask() {
+  var d = new Date()
+
+  var inputVal = document.getElementById('input')
+  // if (checkInputText(inputVal.value.trim(), "Please enter a task")) return;
+  var todo = { id: d.getTime(), value: inputVal.value }
+  todos.push(todo)
+  // store.setItem(todo.id, todo.value);
+  // firebaseRef.child(todo.id).set(todo.value)
+  // console.log(store)
+
+  createElements(todo);
+  inputVal.value = ''
+  inputVal.focus();
+}
+
+function createElements(todo) {
+  var itemRow = document.createElement("div");
+  itemRow.className = "row alert alert-secondary";
+  itemRow.style = "margin-bottom: 0px"
+  itemRow.id = todo.id
+
+  // var itemColoumn = createItemRow(itemRow, todo.value)
+  // var btnbtn = createDeleteButton(itemRow, todo.id)
+  // itemRow.appendChild(btnbtn)
+  // createEditButton(itemRow, itemColoumn, todo, btnbtn)
+  document.getElementById("list-items").appendChild(itemRow)
+}
